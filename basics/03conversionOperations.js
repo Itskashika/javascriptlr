d507e2bfb321b9d9a257650d770c78cb02e76004
Itskashika <maheshwarikashika212@gmail.com>
2026-06-11 15:43:30 +0000

    Addedconversion operations

---
+++ basics/03conversionOperations.js
@@ -0,0 +1,10 @@
+let score=false
+console.log(typeof score)
+let scoreInt=Number(score)
+console.log(scoreInt)//Nan not a number
+console.log(typeof scoreInt)
+ let s=""
+ console.log(typeof s)
+ let sBool=Boolean(s)
+ console.log(sBool)//Nan not a number
+ console.log(typeof sBool)
