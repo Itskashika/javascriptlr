31308bc4e7eb30366a22c2edd30a8898b21ef556
Itskashika <maheshwarikashika212@gmail.com>
2026-06-11 13:40:01 +0000

    Added JS variables and datatypes examples

---
+++ basics/01variables.js
@@ -0,0 +1,8 @@
+const accountId=1233;
+let accEmail="kamil121@gmail.com"
+var accountPassword="12345"
+accountCity="Kanpur"
+//accountId=321 //not allowed
+let accountState;//be default value is undefined
+console.log(accountId)
+console.table([accountId,accountPassword,accEmail,accountCity,accountState])
---
+++ basics/02datatypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code as newer version
+//alert(3+5)// we are using node js not browser
+let name="kashika"
+let age=22
+let isLoggedIn =false
+let city;
+let email=null
+console.table([name,age,isLoggedIn,city,email])
+console.log(typeof "lalit")
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof(age))
