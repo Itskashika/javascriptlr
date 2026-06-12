d1070e28769565925e1d66a34343e88227e7648b
Itskashika <maheshwarikashika212@gmail.com>
2026-06-12 17:48:27 +0000

    Added JS comparisions

--- basics/03conversionOperations.js
+++ basics/03conversionOperations.js
@@ -1,10 +1,31 @@
+
 let score=false
-console.log(typeof score)
+//console.log(typeof score)
 let scoreInt=Number(score)
-console.log(scoreInt)//Nan not a number
-console.log(typeof scoreInt)
+//console.log(scoreInt)//Nan not a number
+//console.log(typeof scoreInt)
  let s=""
- console.log(typeof s)
+ //console.log(typeof s)
  let sBool=Boolean(s)
- console.log(sBool)//Nan not a number
- console.log(typeof sBool)
+ //console.log(sBool)//Nan not a number
+ //console.log(typeof sBool)
+ //-------------Operations---------------
+let val=3
+let negval=-val
+//console.log(negval)
+//console.log(2%3)
+
+let st1=" hello "
+let st2="kia"
+let st3=st1+st2
+//console.log(st3)
+// console.log("1"+2)
+// console.log(2+"1")
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+// if string first then print the string and if operation first then perform operation
+// console.log(true)//true 
+// console.log(+true)//1
+// console.log(+"")
+// console.log(false)
+// console.log(+false)
---
+++ basics/04comparision.js
@@ -0,0 +1,10 @@
+// // console.log(2>1)
+// //console.log("2">1)
+// console.log(null>0)//false
+// console.log(null<=0)//true
+// console.log(null ==0)//false
+// console.log(null>=0)//true
+
+//=== -> Strict check also check data type
+console.log("2"==2)
+console.log("2"===2)
